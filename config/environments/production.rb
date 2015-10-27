28b189b8d9d509f7b771910e95a5efb1c83d5bfd
Wilfredo Lopez <jrlopez85@gmail.com>
2015-10-27 19:50:08 -0400

    Modified production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    region: 'us-east-1'
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']

97188d00733d228e26ab9d3f2b751fc024287aa8
Wilfredo Lopez <jrlopez85@gmail.com>
2015-10-27 20:33:06 -0400

    .

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,8 @@
 # Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 
 # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
+
+Paperclip::Attachment.default_options.merge!(
+       :url => ':s3_domain_url',
+       :path => 'app/public/:class/:attachment/:id_partition/:style/:filename'
+   )

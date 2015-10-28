339e60f6ec71c862c788411b907f92dc61195faa
Wilfredo Lopez <jrlopez85@gmail.com>
2015-10-28 10:28:08 -0400

    Created and replaced new bucket in AWS

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@
 
 # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
 
-Paperclip::Attachment.default_options.merge!(
-       :url => ':s3_domain_url',
-       :path => 'app/public/:class/:attachment/:id_partition/:style/:filename'
-   )
+# Paperclip::Attachment.default_options.merge!(
+#        :url => ':s3_domain_url',
+#        :path => 'app/public/:class/:attachment/:id_partition/:style/:filename'
+#    )
